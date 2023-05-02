65c46224e2d812814a789bff3ef3406c9b890dcc
shrisrivastav <shrijitsrivastav@gmail.com>
2023-05-02 21:21:36 +0530

    april1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+// route 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+    res.send('Hello!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
